ee5cfa133dc14d2123682cc62f4f642b241ceafa
aLuizab <aluiza.primo@gmail.com>
2020-10-16 00:27:46 -0300

    inserindo banco de dados sqlite

--- models/usuario.py
+++ models/usuario.py
@@ -1,12 +1,33 @@
-class UsuarioModel:
-    def __init__(self, nome, sobrenome, cpf):
+from sql_alchemy import banco
+
+
+class UsuarioModel(banco.Model):
+    __tablename__ = 'usuarios'
+
+    cpf = banco.Column(banco.Integer, primary_key=True)
+    nome = banco.Column(banco.String(80))
+    sobrenome = banco.Column(banco.String(80))
+    email = banco.Column(banco.String(80))
+    nascimento = banco.Column(banco.Integer)
+    id = banco.Column(banco.Integer)
+
+
+
+    def __init__(self, cpf, nome, sobrenome, email, nascimento, id):
+        self.cpf = cpf
         self.nome = nome
         self.sobrenome = sobrenome
-        self.cpf = cpf
+        self.email = email
+        self. nascimento = nascimento
+        self.id = id
+
 
     def json(self):
         return {
+            'cpf':self.cpf,
             'nome': self.nome,
             'sobrenome': self.sobrenome,
-            'cpf': self.cpf
+            'email': self.email,
+            'nascimento': self.nascimento,
+            'id': self.id
         }
--- resources/usuario.py
+++ resources/usuario.py
@@ -3,17 +3,17 @@ from models.usuario import UsuarioModel
 
 usuarios = [
     {
+    'cpf': 123456,
     'nome': 'Ana',
     'sobrenome': 'Botelho',
-    'cpf': 123456,
     'email': 'ana@gmail.com',
     'nascimento': '16/12/1997',
     'id': 1
     },
 {
+    'cpf': 789123,
     'nome': 'Luiza',
     'sobrenome': 'Primo',
-    'cpf': 789123,
     'email': 'luiza@gmail.com',
     'nascimento': '16/12/1997',
     'id': 2
@@ -28,43 +28,47 @@ class Usuarios(Resource):
 class Usuario(Resource):
     #argumentos passados para os dados do usuario
     argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
     argumentos.add_argument('sobrenome')
-    argumentos.add_argument('cpf')
+    argumentos.add_argument('email')
+    argumentos.add_argument('nascimento')
+    argumentos.add_argument('id')
 
 
-    #verifica se o usuraio ja existe
-    def find_usuario(nome):
+
+    #verifica se o usuraio ja existe pelo cpf
+    def find_usuario(cpf):
         for usuario in usuarios:
-            if usuario['nome'] == nome:
+            if usuario['cpf'] == cpf:
                 return usuario
-    #retorna usuario pelo nome
-    def get(self, nome):
-        usuario = Usuario.find_usuario(nome)
-        if usuario:
+    #retorna usuario pelo cpf
+    def get(self, cpf):
+        usuario = Usuario.find_usuario(cpf)
+        if cpf:
             return usuario
         return {'message': "Usuario nao encontrado"}, 404 #not found
-    #cria novo usuario
-    def post(self, nome):
+    #cria novo usuario pelo cpf
+    def post(self, cpf):
         dados = Usuario.argumentos.parse_args()
-        usuario_objeto = UsuarioModel(nome, **dados)
+        usuario_objeto = UsuarioModel(cpf, **dados)
         novo_usuario = usuario_objeto.json()
         usuarios.append(novo_usuario)
         return novo_usuario, 200
 
-    #editar usuario ou criar novo
-    def put(self, nome):
+    #editar usuario ou criar novo pelo cpf
+    def put(self, cpf):
         dados = Usuario.argumentos.parse_args()
-        usuario_objeto = UsuarioModel(nome, **dados)
+        usuario_objeto = UsuarioModel(cpf, **dados)
         novo_usuario = usuario_objeto.json()
-        usuario = Usuario.find_usuario(nome)
-        if usuario:
+        usuario = Usuario.find_usuario(cpf)
+        if cpf:
             usuario.update(novo_usuario)
             return novo_usuario, 200 #OK
         usuarios.append(novo_usuario)
         return novo_usuario, 201 #created
 
-
-    def delete(self, nome):
+    #deletar usuario pelo cpf
+    def delete(self, cpf):
         global usuarios
-        usuarios = [usuario for usuario in usuarios if usuario['nome'] != nome]
+        usuarios = [usuario for usuario in usuarios if usuario['cpf'] != cpf]
         return {'message': 'Usuario deletado'}
