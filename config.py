1252a3d38d2756a732323a64d3a12e1ca75b8d6c
aLuizab <aluiza.primo@gmail.com>
2020-10-16 07:18:46 -0300

    consertando arquivo

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI - os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     MAIL_SERVER = 'aluiza.primo@gmail.com'
     MAIL_PORT = 587
