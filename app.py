f9d401f779c81cdc92842286812b2aab27625262
aLuizab <aluiza.primo@gmail.com>
2020-10-16 09:53:32 -0300

    tentativa de conexão com o banco

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from resources.usuario import Usuarios, Usuario
+import os
+import psycopg2
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sligvgaxcuplpn:9e8446c8f5f25489a9b91f8566987ac7472fcb4b06bd2400e9d6575f965d46f2@ec2-35-174-127-63.compute-1.amazonaws.com:5432/d8mq86ue2m3lah'
+DATABASE_URL = os.environ['postgres://sligvgaxcuplpn:9e8446c8f5f25489a9b91f8566987ac7472fcb4b06bd2400e9d6575f965d46f2@ec2-35-174-127-63.compute-1.amazonaws.com:5432/d8mq86ue2m3lah']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sligvgaxcuplpn:9e8446c8f5f25489a9b91f8566987ac7472fcb4b06bd2400e9d6575f965d46f2@ec2-35-174-127-63.compute-1.amazonaws.com:5432/d8mq86ue2m3lah'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
