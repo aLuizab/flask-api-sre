d3de4636b8688cc2ffc3a3e230795af46d3b207a
aLuizab <aluiza.primo@gmail.com>
2020-10-16 09:56:45 -0300

    tentativa de conexão com o banco

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import psycopg2
 
 
 app = Flask(__name__)
-DATABASE_URL = os.environ['postgres://sligvgaxcuplpn:9e8446c8f5f25489a9b91f8566987ac7472fcb4b06bd2400e9d6575f965d46f2@ec2-35-174-127-63.compute-1.amazonaws.com:5432/d8mq86ue2m3lah']
+DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sligvgaxcuplpn:9e8446c8f5f25489a9b91f8566987ac7472fcb4b06bd2400e9d6575f965d46f2@ec2-35-174-127-63.compute-1.amazonaws.com:5432/d8mq86ue2m3lah'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
